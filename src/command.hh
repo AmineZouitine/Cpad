dbae11a268a50b2f9780836fba856a748e04bf27
Amine Zouitine <amine.zouitine@epita.fr>
2022-04-21 13:21:37 +0200

    fix: fix clang

--- src/command.hh
+++ src/command.hh
@@ -5,7 +5,7 @@
 
 class Command : public Element {
 public:
-  Command(std::string &command_name);
-  std::string &get_command_name() const;
-  void execute_command() const;
+    Command(std::string &command_name);
+    std::string &get_command_name() const;
+    void execute_command() const;
 };
