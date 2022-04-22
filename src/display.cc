e4522802d41996c13fc9833b263d76b26ce26c3c
Oceane Valat <oceane.valat@epita.fr>
2022-04-22 23:01:48 +0200

    display with colors

--- src/display.cc
+++ src/display.cc
@@ -1,14 +1,30 @@
 #include "display.hh"
 
+#define RESET "\033[0m"
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDRED "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m"      /* Bold Green */
+#define YELLOW "\033[33m"      /* Yellow */
+
+
 void Display::display (std::string &key, std::map<std::string, Folder> &map)
 {
     //HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    std::cout << "Current folder : " << key << std::endl;
+    std::cout << "Current folder : " << BOLDGREEN << key << std::endl << std::endl;
+    std::cout << RESET;
     int i = 1;
     auto elms = map[key].get_elements();
     for(auto elm : elms)
     {
-        std::cout << std::to_string(i) << "- " << elm.get_name() << std::endl;
+        if (elm.get_is_folder())
+            std::cout << std::to_string(i) << "- " << BOLDBLUE << elm.get_name() << std::endl;
+        else 
+            std::cout << std::to_string(i) << "- " << elm.get_name() << std::endl;
+        std::cout << RESET;
         i++;
     }
+    std::cout << YELLOW << "q- quit cpad" << std::endl;
+    std::cout << RESET;
+    std::cout << std::endl;
+
 }
