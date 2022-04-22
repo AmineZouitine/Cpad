262fa619ad0d80cc24bcf388f64ee10c51e056d9
Amine Zouitine <amine.zouitine@epita.fr>
2022-04-22 16:18:25 +0200

    parser: add command test

--- src/convertor.hh
+++ src/convertor.hh
@@ -11,4 +11,10 @@ class Convertor : public Singleton<Convertor>
 public:
     std::map<std::string, Folder> read(std::string& path);
     void write(std::map<std::string, Folder>& map, std::string& path);
+    void add_command(std::map<std::string, Folder>& map, std::string& key, std::string& command);
+    void add_folder(std::map<std::string, Folder>& map, std::string& key, std::string& folder_name);
+    void remove_command(std::map<std::string, Folder>& map, std::string& key, size_t command);
+    void remove_folder(std::map<std::string, Folder>& map, std::string& key);
+    void reset_folder(std::map<std::string, Folder>& map, std::string& key);
+    void reset_all(std::map<std::string, Folder>& map);
 };
--- tests/convertor-tests.hh
+++ tests/convertor-tests.hh
@@ -118,3 +118,41 @@ TEST(convertor, write_medium)
     ASSERT_FALSE(map["git commands"].get_elements()[0].get_is_folder());
     ASSERT_EQ(map["git commands"].get_elements()[0].get_name(), "git add *");
 }
+
+TEST(convertor, write_add_command)
+{
+    std::string path1 = "./test-ressources/.cpad_medium";
+    std::string path =
+        "./test-ressources/.cpad_medium_test1";
+
+    auto map = Convertor::instance().read(path1);
+
+    std::string parent_folder = ".";
+    std::string command_name = "echo new command";
+    Convertor::instance().add_command(map, parent_folder, command_name);
+
+    Convertor::instance().write(map, path);
+
+    ASSERT_EQ(map.size(), 2);
+
+    ASSERT_TRUE(map.find(".") != map.end());
+    ASSERT_EQ(map["."].get_elements().size(), 4);
+
+    ASSERT_TRUE(map["."].get_elements()[0].get_is_folder());
+    ASSERT_EQ(map["."].get_elements()[0].get_name(), "git commands");
+
+    ASSERT_TRUE(map["."].get_elements()[1].get_is_folder());
+    ASSERT_EQ(map["."].get_elements()[1].get_name(), "test");
+
+    ASSERT_FALSE(map["."].get_elements()[2].get_is_folder());
+    ASSERT_EQ(map["."].get_elements()[2].get_name(), "ls -a");
+
+    ASSERT_FALSE(map["."].get_elements()[3].get_is_folder());
+    ASSERT_EQ(map["."].get_elements()[3].get_name(), "ls");
+
+    ASSERT_TRUE(map.find("git commands") != map.end());
+    ASSERT_EQ(map["git commands"].get_elements().size(), 1);
+
+    ASSERT_FALSE(map["git commands"].get_elements()[0].get_is_folder());
+    ASSERT_EQ(map["git commands"].get_elements()[0].get_name(), "git add *");
+}
